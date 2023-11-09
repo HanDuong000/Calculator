f331fd38144f5a1e15c5afe5ee501397d7851795
ngoc han <2154050087han@ou.edu.vn>
2023-11-09 20:58:28 +0700

    Xu ly nut Cong

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -18,7 +18,10 @@ namespace WindowsFormsApplication1
 
         private void btnCong_Click(object sender, EventArgs e)
         {
-
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKQ.Text = c.ToString();
         }
     }
 }
